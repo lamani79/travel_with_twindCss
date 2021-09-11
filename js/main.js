e6b853a305d70aec95cc4e8c4195aa86d2433ec7
Khelil Yasser Djamel Eddie <lmutsuha79@gmail.com>
2021-09-11 21:45:37 +0100

    add files

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+const mobile_nav = document.getElementById('mobile_nav');
+document.getElementById('nav_btn').addEventListener('click',toggle_nav_effects);
+document.getElementById('close_nav_btn').addEventListener('click',toggle_nav_effects);
+
+function toggle_nav_effects(){
+    toggle_blur_nav();
+    toggle_mb_nav_height();
+}
+function toggle_blur_nav(){
+    container.classList.toggle('blur-sm');
+}
+function toggle_mb_nav_height(){
+    if(mobile_nav.style.height == '0px'){
+
+        mobile_nav.style.height = '400px'
+    }else{
+        mobile_nav.style.height = '0px'
+
+    }
+}
+// only for test
+// window.addEventListener('load',toggle_nav_effects)
+
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      zIndex: {
+        '-1' : '-1',
+      },
+      fontFamily:{
+        'pfont' : ['Poppins', 'sans-serif'],
+        "hfont" : ['Volkhov', 'serif']
+      },
+      colors: {
+        'blacki' : '#212832',
+        'redi' : '#DF6951',
+        'orangi' : '#F1A501',
+        'pcolor' : '#5E6282',
+        'hcolor' : '#181E4B'
+      },
+      spacing: {
+        'for10' : '0.63rem',
+      }
+
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
